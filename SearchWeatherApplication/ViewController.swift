c7697ca0672a4c3b8b9c1f9911ed8792421b1ead
bome24 <81296575+bome24@users.noreply.github.com>
2023-03-08 19:17:14 +0900

    FEAT: Initial UI Setting

---
+++ SearchWeatherApplication/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SearchWeatherApplication
+//
+//  Created by BoMin on 2023/03/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var citySearchTextField: UITextField!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
