240f2a4f84af57c400214cfd3920b1858914c1b5
bome24 <81296575+bome24@users.noreply.github.com>
2023-03-08 22:19:47 +0900

    FEAT: URLRequest Extensions

---
+++ SearchWeatherApplication/Extensions/URLRequest+Extensions.swift
@@ -0,0 +1,30 @@
+//
+//  URLRequest+Extensions.swift
+//  SearchWeatherApplication
+//
+//  Created by BoMin on 2023/03/08.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct Resource<T> {
+    let url: URL
+}
+
+extension URLRequest {
+    
+    static func load<T: Decodable>(resource: Resource<T>) -> Observable<T> {
+        
+        return Observable.from([resource.url])
+            .flatMap { url -> Observable<Data> in
+                let request = URLRequest(url: url)
+                return URLSession.shared.rx.data(request: request)
+            }.map { data -> T in
+                return try JSONDecoder().decode(T.self, from: data)
+            }.asObservable()
+        
+    }
+    
+}
